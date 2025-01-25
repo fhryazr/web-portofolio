6061dbe39e593a1ecd70691d08cb75e9116d5432
fhryazr <fahryazaria@gmail.com>
2025-01-25 10:33:05 +0800

    style(home): animete hero section

--- src/pages/HeroSection.tsx
+++ src/pages/HeroSection.tsx
@@ -1,11 +1,12 @@
 import CustomButton from "@/components/CustomButton";
+import "@/App.css";
 
 const HeroSection = () => {
   return (
     <>
       <section className="flex min-h-[90vh] flex-col items-center justify-center gap-6 bg-[#F0EFEB]">
         <div>
-          <div className="relative ml-[-5rem] h-[5rem] w-[5rem] overflow-hidden rounded-bl-full rounded-tl-full rounded-tr-full bg-[#449F91] shadow-lg shadow-neutral-600">
+          <div className="relative ml-[-5rem] h-[5rem] w-[5rem] animate-bounce overflow-hidden rounded-bl-full rounded-tl-full rounded-tr-full bg-[#449F91] shadow-lg shadow-neutral-500">
             <img
               style={{
                 height: "100%",
@@ -17,14 +18,19 @@ const HeroSection = () => {
               alt="my-picture"
             />
           </div>
-          <p className="w-fit rounded-full bg-white px-4 py-2 text-center font-[Sora] text-lg font-medium shadow-xl">
+          <p className="animate-chatIn w-fit rounded-bl-full rounded-br-full rounded-tr-full bg-white px-4 py-2 text-center font-[Sora] text-lg font-medium shadow-xl">
             Hi, I'm Fachry 😎
           </p>
         </div>
-        <h1 className="text-center text-[5rem] leading-[6rem]">
-          <b>Front End Developer</b>
+        <h1
+          className="text-center font-[Sora] text-[5rem] leading-[6rem]"
+          style={{
+            textShadow: "2px 2px 3px rgba(0, 0, 0, 0.2)", // Shadow hitam
+          }}
+        >
+          <b className="text-[#449F91]">Front End Developer</b>
           <br />
-          Based in <b>Indonesia</b>
+          Based in <b className="text-shadow text-[#FFAF12]">Indonesia</b>
         </h1>
         <p className="mb-5 text-center text-lg font-light text-[#a09c95]">
           I would be happy to help create a user-friendly and intuitive website
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ["class"],
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: ["class"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-  	extend: {
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {}
-  	}
+    extend: {
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        breathe: {
+          "0%, 100%": { transform: "scale(1)" },
+          "50%": { transform: "scale(1.05)" },
+        },
+        chatInOut: {
+          "0%": {
+            transform: "translateY(-30px) translateX(-90px) scale(0)",
+            opacity: "0",
+          }, // Start (fade-in)
+          "8%": { transform: "translateY(0)", opacity: "1" }, // Fully visible
+          "92%": { transform: "translateY(0)", opacity: "1" }, // Idle state
+          "100%": {
+            transform: "translateY(-30px) translate(-90px) scale(0)",
+            opacity: "0",
+          }, // Fade-out
+        },
+      },
+      animation: {
+        breathe: "breathe 2s ease-in-out infinite",
+        chatIn: "chatInOut 7s ease-in-out infinite",
+      },
+      colors: {},
+    },
   },
   plugins: [require("tailwindcss-animate")],
 };
